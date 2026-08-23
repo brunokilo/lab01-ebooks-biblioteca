33a8ebf1f5d0060d81f8fe60c739c56ad6bf6299
Bruno Henrique <brunohenfp@gmail.com>
2026-08-23 20:45:53 -0300

    feat: criacao dos atributos e stubs

--- codigo/src/br/edu/pucminas/biblioteca/modelo/Usuario.java
+++ codigo/src/br/edu/pucminas/biblioteca/modelo/Usuario.java
@@ -2,4 +2,22 @@ package br.edu.pucminas.biblioteca.modelo;
 
 public class Usuario {
     
+    private String id;
+    private String nome;
+    private String senha;
+
+
+    public Usuario (String id, String nome, String senha){
+        if(id == null || nome == null || senha == null){
+            throw new IllegalArgumentException("Ocorreu um erro com um dos dados de criação, tente novamente");
+        }
+        
+        this.id = id;
+        this.nome = nome;
+        this.senha = senha;
+    }
+
+    protected void autenticar(){
+        //TODO: Implementar na Sprint 3
+    }
 }
