1be0ad25560e9f5cc7492a939473a0d3e79b081c
Bruno Henrique <brunohenfp@gmail.com>
2026-08-23 20:44:48 -0300

    feat: atributos e stubs das classes Administrador, bibliotecario e Categoria

--- codigo/src/br/edu/pucminas/biblioteca/modelo/Administrador.java
+++ codigo/src/br/edu/pucminas/biblioteca/modelo/Administrador.java
@@ -2,4 +2,12 @@ package br.edu.pucminas.biblioteca.modelo;
 
 public class Administrador extends Usuario {
     
+    
+    public Administrador(String id, String nome, String senha) {
+        super(id, nome, senha);
+    }
+
+    private void gerenciarSenha(){
+        //TODO: Implementar na Sprint 3;
+    }
 }
